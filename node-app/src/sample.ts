daae327b493a9b7dae7c80e7fcb1fe719ac2a79f
RajRatan12 <rajratancse@gmail.com>
2024-05-23 17:47:06 +0530

    interface and classes

--- node-app/src/sample.ts
+++ node-app/src/sample.ts
@@ -139,3 +139,186 @@ const func5: newFunc = (a,b,l) => {
 func5(2,3)
 func5(2,3,4)
 
+const func6 = (a:number,b:number, l:number): number => {
+    return a+b+l;
+}
+func6(2,3,6)
+
+
+// rest operator
+
+const func7 = (...m:number[]) => {
+    return m
+}
+// const func7 = (...m:number[]) : number[] => {
+//     return m
+// }
+
+
+func7(1,2,3,4,5,6,7,8,9,10) 
+
+type func8 = (...m:number[]) => number[]
+const func9: func8 = (...m) => {
+    return m
+}
+
+func9(1,2,3,4,5,6,7,8,9,10)
+
+
+function func10(a:number,b:number): number {
+    return a+b;
+}
+//================================================================================================
+type func11 = (a:number,b:number) => number;
+const func12: func11 = function (a,b){
+    return a+b;
+}
+func12(2,3)
+//================================================================================================
+//function with objects
+
+const getData = (product: {
+    name: string,
+    price: number,
+    stock: number,
+    photo: string
+}):void => {
+    console.log(product);
+}
+
+
+
+type getDataType = (product:{
+    name: string,
+    price: number,
+    stock: number,
+    photo: string
+}) => void
+
+const getData2: getDataType = (product) => {
+    console.log(product);
+}
+
+const product1 = {
+    name: "Laptop",
+    price: 1000,
+    stock: 10,
+    photo: "laptop.jpg"
+}
+getData2(product1)
+getData2({
+    name: "Mobile",
+    price: 500,
+    stock: 20,
+    photo: "mobile.jpg"
+})
+
+// other way to define the object
+//too much repetition
+const product2 : {
+    name: string,
+    price: number,
+    stock: number,
+    
+} = {
+    name: "Laptop",
+    price: 1000,
+    stock: 10,
+    
+}
+
+//================================================================================================
+interface Product {
+    name: string,
+    price: number,
+    stock: number,
+    photo: string,
+    readonly id: number
+}
+
+type getDataType2 = (product: Product) => void
+
+const getData3: getDataType2 = (product) => {
+    console.log(product);
+}
+const product3: Product = {
+    name: "Laptop",
+    price: 1000,
+    stock: 10,
+    photo: "laptop.jpg",
+    id: 1
+}
+
+getData3(product3)
+//================================================================================================
+
+
+// never type
+const errHandler = (): never => {
+    throw new Error();
+    //return new Error();
+}
+
+type thememode = "light" | "dark";
+
+const mode: thememode = "light";
+
+
+//class
+
+class player {
+   // height: number;
+   private height: number;
+   public weight: number;
+    constructor(height: number, weight: number){
+        this.height = height;
+        this.weight = weight;
+    }
+    myHeight=()=>{
+        return this.height;
+    
+    }
+}
+
+const abhi = new player(100,200);
+//console.log(abhi.height, abhi.weight);
+console.log(abhi.myHeight());
+
+//small syntax
+
+class player2 {
+    constructor(private height: number, public weight: number){
+    }
+}
+const abhi2 = new player2(100,200);
+console.log(abhi2.weight);
+
+//=========================================================================================
+class player3 {
+    constructor(private height: number, public weight: number, protected power: number){
+    }
+    getMyheight =()=>this.height
+
+    get getPower(): number{
+        return this.power;
+    }
+    set changePower(power: number){
+        this.power = power;
+    }
+}
+
+const abhi3 = new player3(100,200,300);
+console.log(abhi3.getPower);
+abhi3.changePower = 400;
+console.log(abhi3.getPower);
+
+class player4 extends player3 {
+    constructor(height: number, weight: number, power: number, private age: number){
+        super(height,weight,power);
+    }
+    getAge =()=>this.age;
+}
+const abhi4 = new player4(100,200,300,25);
+console.log(abhi4.getAge());
+console.log(abhi4.getMyheight());
+//=========================================================================================
